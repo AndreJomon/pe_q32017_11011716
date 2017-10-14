9e547208af4b6be94c37d93936573b1177c81abc
Andre Jomon <jomonzito2010@gmail.com>
2017-10-14 19:16:41 -0300

    Ex 13 feito

---
+++ lista02/13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int mdc (int a, int b, int r)
+{
+	if (b == 0){
+		return a;
+	}
+	r = a%b;
+	a = b;
+	b = r;
+	return mdc(a, b, r);
+}
+
+int main ()
+{
+	int a, b, r;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", mdc(a, b, 0));
+	return 0;
+}
