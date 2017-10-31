9f58dcdfd20d289b9efce0b600ad65e6353d0d43
Andre Jomon <jomonzito2010@gmail.com>
2017-10-31 05:30:04 -0200

    Feito ex 6

---
+++ lista03/6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Já incluso*/
+int cmpfunc (const void *a, const void *b)
+{
+	return(*(int*)a-*(int*)b);
+}
+
+/*Calcula mediana*/
+double mediana(int *v, int size)
+{
+	if (size%2 != 0) {
+		return v[(size-1)/2];
+	}
+	return (((double)v[(size-1)/2]+ (double)v[size/2])/2);
+		
+}
+
+int main ()
+{
+	int size;
+	int *v;
+	scanf("%d", &size);
+	v = malloc(size*sizeof(int));
+	for (int i = 0; size>i; i++) {
+		scanf("%d", (v+i));
+	}
+	qsort(v, size, sizeof(int), cmpfunc);
+	printf("%.2lf", mediana(v, size));
+
+	free(v);
+	return 0;
+}
